61b75067dff14ec4c7ded55105be7a486bb823f7
Thibaut Izard <thibaut.izard@gmail.com>
2025-08-01 11:01:34 +0200

    🎨 style: Improve posts styling

---
+++ src/markdown/-components/external-link.tsx
@@ -0,0 +1,7 @@
+import { SquareArrowOutUpRight } from "lucide-react";
+
+function ExternalLink() {
+	return <SquareArrowOutUpRight className="inline-block mr-0.5" />;
+}
+
+export default ExternalLink;
