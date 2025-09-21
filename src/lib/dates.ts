79672659cbe525d7d7b60fc085b6b74fba2b48ef
Thibaut Izard <thibaut.izard@gmail.com>
2025-09-21 12:24:31 +0200

    🔨 refactor: Improve formatDate option

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -2,9 +2,10 @@ export function formatDateForPost(dateString: string) {
   const date = new Date(dateString);
   if (Number.isNaN(date.getTime())) return null;
 
-  const userBrowserLocale = navigator.language;
   const DEFAULT_LOCALE = "fr-FR";
-  const locale = userBrowserLocale || DEFAULT_LOCALE;
+  const userBrowserLocale =
+    typeof navigator !== "undefined" ? navigator.language : null;
+  const locale = userBrowserLocale ?? DEFAULT_LOCALE;
 
   return date.toLocaleDateString(locale, {
     year: "numeric",
